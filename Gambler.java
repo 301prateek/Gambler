fabf8078697471503b58fba5b155dbad2532a3d8
301prateek <prateek.patil301@gmail.com>
2020-12-03 10:43:31 +0530

    Added: Win Or Loose After Bet

--- Gambler.java
+++ Gambler.java
@@ -1,5 +1,20 @@
 public class Gambler {
 
-	int totalMoney = 100;
-	int betPerGame = 1;
+	public static void main(String[] args) {
+		int totalMoney = 100;
+		int betPerGame = 1;
+
+		double random = Math.floor(Math.random() * 2);
+
+		if( random == 1) {
+			betPerGame += 1;
+			totalMoney += betPerGame;
+			System.out.println("Win => "+betPerGame+" TotalMoney => "+totalMoney);
+		}
+		else {
+			betPerGame -= 1;
+			totalMoney = totalMoney - 1;
+			System.out.println("Loose => "+betPerGame+" TotalMoney => "+totalMoney);
+		}
+	}
 }

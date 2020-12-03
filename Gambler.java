d685def67d72faf9bc00d8e5fb85b174b57b16e7
301prateek <prateek.patil301@gmail.com>
2020-12-03 13:13:19 +0530

    Refactor: Counting Total Wins And Losses At End Of Month

--- Gambler.java
+++ Gambler.java
@@ -6,14 +6,19 @@ public class Gambler {
 		int totalWon = 0;
 		int totalLost = 0;
 		int totalDays = 0;
-		
-		while(totalDays < 20) {
-			double random = Math.floor(Math.random() * 2);
-	
+		int amtAfterWin = 0;
+		int amtAfterLoss = 0;
+		int wins = 0;
+		int losses = 0;
+
+		while(totalDays < 30) {
+			int random = (int) Math.floor(Math.random() * 2);
+			
 			if( random == 1) {
 				betPerGame += 1;
 				totalWon = totalMoney + betPerGame;
 				totalDays += 1;
+				wins += 1;
 				System.out.println("Day : "+totalDays);
 				System.out.println("Win => "+betPerGame);
 				System.out.println("Total Win => "+totalWon);
@@ -23,10 +28,15 @@ public class Gambler {
 				betPerGame -= 1;
 				totalLost = totalMoney - betPerGame;
 				totalDays += 1;
+				losses += 1;
 				System.out.println("Day : "+totalDays);
 				System.out.println("Loose => "+betPerGame);
 				System.out.println("Total Lost => "+totalLost);
 			}
 		}
+		amtAfterWin = totalMoney + totalWon;
+		System.out.println("Total Wins : "+wins+", Amount after Winnings in 30 days : "+amtAfterWin);
+		amtAfterLoss = totalMoney - totalLost;
+		System.out.println("Total Lost: "+losses+", Amount after Losing in 30 days : "+amtAfterLoss);
 	}
 }

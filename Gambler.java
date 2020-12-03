10d5aeabb101a93339dee2ccc202c3126c49956d
301prateek <prateek.patil301@gmail.com>
2020-12-03 11:41:42 +0530

    Refactor: Program To Run Untill Gambler Losses or Wins 50 Percent Of Amount

--- Gambler.java
+++ Gambler.java
@@ -3,18 +3,24 @@ public class Gambler {
 	public static void main(String[] args) {
 		int totalMoney = 100;
 		int betPerGame = 1;
-
-		double random = Math.floor(Math.random() * 2);
-
-		if( random == 1) {
-			betPerGame += 1;
-			totalMoney += betPerGame;
-			System.out.println("Win => "+betPerGame+" TotalMoney => "+totalMoney);
-		}
-		else {
-			betPerGame -= 1;
-			totalMoney = totalMoney - 1;
-			System.out.println("Loose => "+betPerGame+" TotalMoney => "+totalMoney);
+		int totalWon = 0;
+		int totalLost = 0;
+		
+		while(totalLost <= 50 || totalWon < 150) {
+			double random = Math.floor(Math.random() * 2);
+	
+			if( random == 1) {
+				betPerGame += 1;
+				totalWon = totalMoney + betPerGame;
+				System.out.println("Win => "+betPerGame);
+				System.out.println("Total Win => "+totalWon);
+			}
+			else {
+				betPerGame -= 1;
+				totalLost = totalMoney - betPerGame;
+				System.out.println("Loose => "+betPerGame);
+				System.out.println("Total Lost => "+totalLost);
+			}
 		}
 	}
 }

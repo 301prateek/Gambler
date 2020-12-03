7444995219350ebd4e6f01768bacdadb69a13da8
301prateek <prateek.patil301@gmail.com>
2020-12-03 11:54:50 +0530

    Refactor: Program To Show Total Amount Lost Or Won Untill 20 Days

--- Gambler.java
+++ Gambler.java
@@ -5,19 +5,25 @@ public class Gambler {
 		int betPerGame = 1;
 		int totalWon = 0;
 		int totalLost = 0;
+		int totalDays = 0;
 		
-		while(totalLost <= 50 || totalWon < 150) {
+		while(totalDays < 20) {
 			double random = Math.floor(Math.random() * 2);
 	
 			if( random == 1) {
 				betPerGame += 1;
 				totalWon = totalMoney + betPerGame;
+				totalDays += 1;
+				System.out.println("Day : "+totalDays);
 				System.out.println("Win => "+betPerGame);
 				System.out.println("Total Win => "+totalWon);
+				
 			}
 			else {
 				betPerGame -= 1;
 				totalLost = totalMoney - betPerGame;
+				totalDays += 1;
+				System.out.println("Day : "+totalDays);
 				System.out.println("Loose => "+betPerGame);
 				System.out.println("Total Lost => "+totalLost);
 			}
